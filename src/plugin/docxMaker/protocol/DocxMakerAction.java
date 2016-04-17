3e8a28b76a041e63dfc81bea323e27535458879a
Jay-Liao <j99590314@gmail.com>
2016-04-17 23:01:50 +0800

    fixed this plug-in compatible on ezScrum 1.8.0

--- src/plugin/docxMaker/protocol/DocxMakerAction.java
+++ src/plugin/docxMaker/protocol/DocxMakerAction.java
@@ -5,18 +5,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
-import ntut.csie.ezScrum.pic.core.IUserSession;
-import ntut.csie.ezScrum.web.form.ProjectInfoForm;
-import ntut.csie.ezScrum.web.internal.IProjectSummaryEnum;
-import ntut.csie.protocal.Action;
 
 import org.codehaus.jettison.json.JSONException;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
+import ntut.csie.protocal.Action;
 import plugin.docxMaker.webservice.EzScrumWebServiceController;
 
 public class DocxMakerAction implements Action {
@@ -27,18 +27,17 @@ public class DocxMakerAction implements Action {
 
 	public void doGetReleasePlan(StaplerRequest request, StaplerResponse response) {
 		HttpSession session = request.getSession();
-		ProjectInfoForm projectInfoForm = (ProjectInfoForm) session.getAttribute(IProjectSummaryEnum.PROJECT_INFO_FORM);
-		IUserSession userSession = (IUserSession) session.getAttribute("UserSession");
-		String userName = userSession.getAccount().getAccount();
 		String encodedPassword = (String) session.getAttribute("passwordForPlugin");
-		String projectId = projectInfoForm.getName();
+		String projectName = getURLParameter(request, "projectName");
 		String releaseId = request.getParameter("releaseID");
+		Cookie[] cookies = request.getCookies();
+		String encodedUserName = cookies[1].getValue();
 		InputStream in = null;
 		PrintWriter writer = null;
 		try {
 			String ezScrumURL = request.getServerName() + ":" + request.getLocalPort() + request.getContextPath();
 			EzScrumWebServiceController service = new EzScrumWebServiceController(ezScrumURL);
-			File releasePlan = service.getReleaseDocx(userName, encodedPassword, projectId, releaseId);
+			File releasePlan = service.getReleaseDocx(encodedUserName, encodedPassword, projectName, releaseId);
 			response.setHeader("Content-Transfer-Encoding", "binary");
 			response.setHeader("Content-Disposition", "attachment; filename=" + releasePlan.getName());
 			response.setContentLength((int) releasePlan.length());
@@ -60,5 +59,43 @@ public class DocxMakerAction implements Action {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	private static String getURLParameter(HttpServletRequest request, String paramName) {
+		try {
+			/**
+			 * 拿到request header的URL parameter 如果拿不到referer資訊此時回傳null
+			 */
+			String referer = request.getHeader("Referer");
+			if (referer == null) return null;
+			// 將URL轉換成沒有萬用字元符號
+			String url = URLDecoder.decode(referer, "UTF-8");
+
+			/**
+			 * 如果用 "?"拆除出來的params是null或只有一個urlParams 代表網址後面沒有帶參數 此時回傳null
+			 */
+			String[] urlParams = url.split("\\?");
+			if (urlParams == null || urlParams.length <= 1) return null;
+
+			// 取得URL 問號後面的Parameter參數
+			url = urlParams[1];
+			// params 裡面存的大概像這樣 [{id=15}, {op=55}]，為各個parameter
+			String[] params = url.split("&");
+			// value 用來接params的 parameter的key跟value ex: [id, 15]
+			String[] value;
+
+			/**
+			 * 找到對應的parameter name並把value回傳給使用者
+			 */
+			for (int i = 0; i < params.length; i++) {
+				value = params[i].split("=");
+				if (value[0].equals(paramName)) {
+					return value[1];
+				}
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return paramName;
 	}
 }
--- src/plugin/docxMaker/webservice/EzScrumWebServiceController.java
+++ src/plugin/docxMaker/webservice/EzScrumWebServiceController.java
@@ -1,27 +1,17 @@
 package plugin.docxMaker.webservice;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
 
 import javax.ws.rs.core.MediaType;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
-import plugin.docxMaker.DocxMaker;
-import plugin.docxMaker.dataModel.ReleasePlanObject;
-import plugin.docxMaker.dataModel.SprintPlanObject;
-import plugin.docxMaker.dataModel.StoryObject;
-import plugin.docxMaker.dataModel.TaskObject;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.WebResource.Builder;
-import com.sun.jersey.core.util.Base64;
+
+import plugin.docxMaker.DocxMaker;
 
 public class EzScrumWebServiceController {
 	private String mEzScrumURL;
@@ -33,34 +23,25 @@ public class EzScrumWebServiceController {
 	/**
 	 * use web-service to get release info and return release plan file
 	 * 
-	 * @param account - the user account(not encoded)
+	 * @param encodedUserName - the user account(encoded)
 	 * @param encodedPassword - the user password(encoded)
 	 * @param projectId - the release of project
 	 * @param releaseId - the release docx you wnat
 	 * @return release docx file
 	 * @throws JSONException
 	 */
-	public File getReleaseDocx(String account, String encodedPassword, String projectId, String releaseId) throws JSONException {
-		String encodedUserName = new String(Base64.encode(account.getBytes()));
-		// 需要的帳密為暗碼
+	public File getReleaseDocx(String encodedUserName, String encodedPassword, String projectId, String releaseId) throws JSONException {
 		StringBuilder releaseWebServiceUrl = new StringBuilder();
 		releaseWebServiceUrl.append("http://")
 					        .append(mEzScrumURL)
 					        .append("/web-service/").append(projectId)
 					        .append("/release-plan/").append(releaseId)
-					        .append("/all/").append("?userName=").append(encodedUserName).append("&password=").append(encodedPassword);
+					        .append("/all").append("?username=").append(encodedUserName).append("&password=").append(encodedPassword);
 
 		Client client = Client.create();
 		WebResource webResource = client.resource(releaseWebServiceUrl.toString());
 		Builder result = webResource.type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
-		JSONObject resultJSONObject = result.get(JSONObject.class);
-		Gson gson = new Gson();
-		ReleasePlanObject reDesc = gson.fromJson(resultJSONObject.getString("releasePlanDesc"), ReleasePlanObject.class);
-		List<SprintPlanObject> sprinDescList = gson.fromJson(resultJSONObject.getString("sprintDescList"), new TypeToken<List<SprintPlanObject>>() {}.getType());
-		HashMap<String, List<StoryObject>> stories = gson.fromJson(resultJSONObject.getString("stories"), new TypeToken<HashMap<String, List<StoryObject>>>() {}.getType());
-		LinkedHashMap<String, List<TaskObject>> taskMap = gson.fromJson(resultJSONObject.getString("taskMap"), new TypeToken<LinkedHashMap<String, List<TaskObject>>>() {}.getType());
-		HashMap<String, Float> tatolStoryPoints = gson.fromJson(resultJSONObject.getString("totalStoryPoints"), new TypeToken<HashMap<String, Float>>() {}.getType());
-
-		return new DocxMaker().getReleasePlanDocx(reDesc, sprinDescList, stories, taskMap, tatolStoryPoints);
+		JSONObject releaseJSON = result.get(JSONObject.class);
+		return new DocxMaker().getReleasePlanDocx(releaseJSON);
 	}
 }
